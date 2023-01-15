da5eea98ceef92f44933ebbf0c0d543f0fb341a8
Xisurthros <Jacob_Hobbs548@yahoo.com>
2023-01-15 13:10:02 -0600

    First item added

--- src/main/java/net/xisurthros/xisurthros/Xisurthros.java
+++ src/main/java/net/xisurthros/xisurthros/Xisurthros.java
@@ -8,6 +8,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.xisurthros.xisurthros.item.ModItems;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -21,6 +22,8 @@ public class Xisurthros
     public Xisurthros()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
